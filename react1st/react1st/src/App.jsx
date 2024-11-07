215960c96ba4b64c081cee684c6f09a660a16ba5
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-07 19:24:38 +0530

    card

--- react1st/react1st/src/App.jsx
+++ react1st/react1st/src/App.jsx
@@ -1,4 +1,13 @@
  export const App = ()=>{
-  return <h1>Hello Apurv</h1>
+  return (
+    <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h2>Name: Queen of Tears</h2>
+        <h3>Rating:8.2</h3>
+        <p>Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam ducimus consequuntur porro. Reiciendis fugiat, rerum delectus mollitia quidem nemo!</p>
+    </div>
+  )
 }
 

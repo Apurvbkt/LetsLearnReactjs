696b90f2fbfac2d0f0e73b9178d49bee2e9d3d55
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-08 19:53:55 +0530

    push2

--- react1st/react1st/src/App.jsx
+++ react1st/react1st/src/App.jsx
@@ -17,13 +17,14 @@
 }
 
 const Netcard = ()=> {
+  const name = 'queen of tears'
   return(
     
     <div>
       <div>
           <img src="https://images.ctfassets.net/4cd45et68cgf/Rb6wz7UfbWMR26SsqxxJL/82ba4eb884c1b2ca426a2b250d8afa86/ENUS_QueenofTears-Hae-In_CharacterKA_Vertical_RGB_PRE.jpg_ENUS_QueenofTears-Hae-In_CharacterKA_Vertical_RGB_PRE.jpg?w=1200" alt="" width="300px" height="300px" />
         </div>
-        <h2>Name: Queen of Tears</h2>
+        <h2>Name: {name}</h2>
         <h3>Rating:8.2</h3>
         <p>Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quod aliquam ducimus consequuntur porro. Reiciendis fugiat, <br /> rerum delectus mollitia quidem nemo!</p>
     </div>

249e33fde11c2bf1b4970c27db2d34a452b5cb62
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-10 20:37:57 +0530

    push the netflix card

--- react1st/react1st/src/App.jsx
+++ react1st/react1st/src/App.jsx
@@ -1,17 +1,15 @@
  import React from "react"
- import Netcard ,{ Head, Footer } from "./components/Netcard"
+ import Netcard from "./components/Netcard"
 //  import { head } from "./components/Netcard"
  
  export const App = ()=>{
   return (
     <React.Fragment>
       
-      <Head/>
+    
       <Netcard/>
-      <Netcard/>
-      <Netcard/>
-      <Netcard/>
-      <Footer/>
+      
+    
     </React.Fragment>
     
     

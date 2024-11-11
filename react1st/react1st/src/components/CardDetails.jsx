685e0614b464ad0ef1f66d8c8dbfb431d7ae4ef7
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-11 21:24:59 +0530

    push 3

--- react1st/react1st/src/components/CardDetails.jsx
+++ react1st/react1st/src/components/CardDetails.jsx
@@ -1,17 +1,18 @@
 
 
 const CardDetails = (props)=>{
+    const {e} = props
     return(
         <li >
         <div>
-            <img src={props.e.img_url} alt="" width="40%" height="40%" />
+            <img src={e.img_url} alt="" width="40%" height="40%" />
           </div>
-          <h2>Name: {props.e.name}</h2>
-          <h3>Rating:{props.e.rating}</h3>
-          <p>Summary: {props.e.description}</p>
-          <p>Gerenal : {props.e.genre}</p>
-          <p>Cast: {props.e.cast}</p>
-          <a href={props.e.watch_url} target="_blank">
+          <h2>Name: {e.name}</h2>
+          <h3>Rating:{e.rating}</h3>
+          <p>Summary: {e.description}</p>
+          <p>Gerenal : {e.genre}</p>
+          <p>Cast: {e.cast}</p>
+          <a href={e.watch_url} target="_blank">
             <button>Watch Now</button></a>          
           
           

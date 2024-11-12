0e61bb66eca6c4a831d03f6975c27e2122136f96
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-12 22:45:14 +0530

    push 2

--- react1st/react1st/src/components/CardDetails.jsx
+++ react1st/react1st/src/components/CardDetails.jsx
@@ -14,7 +14,7 @@ const CardDetails = ({e})=>{
           <p>Cast: {cast}</p>
           <a href={watch_url} target="_blank">
             <button>Watch Now</button></a>    
-            <p></p>      
+            <p>aaj ni ho paya</p>      
           
           
       </li>

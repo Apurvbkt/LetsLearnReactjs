9f1c17b64ad146e6873dc2489c952d3fdf417b9e
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-15 21:20:16 +0530

    psuh 4

--- react1st/react1st/src/components/CardDetails.jsx
+++ react1st/react1st/src/components/CardDetails.jsx
@@ -20,7 +20,7 @@ const CardDetails = ({e})=>{
                 <p> no more tme pass</p>
                 <p>good night </p>
                 <p>sorry for delay aaj bhi ni hua, kal se for sure pdhai suru</p>
-                <p> coz of work, aaj bhi ni ho paya</p>
+                <p> coz of work, aaj bhi ni ho paya, I dont know till this week</p>
                 
           
           

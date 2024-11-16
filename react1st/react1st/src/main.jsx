44da7fa4e71b63f09ddb417caebd066d3d096026
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-16 19:06:48 +0530

    push 4

--- react1st/react1st/src/main.jsx
+++ react1st/react1st/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import {App} from './App.jsx'
-import ProfileCard from './components/ProfileCard.jsx'
+import Profile from './components/ProfileCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <ProfileCard/>
+    <Profile/>
   </StrictMode>,
 )

7d9e1ad012706e075dee55c9e52ec5f33e57a0c5
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-17 22:52:32 +0530

    push 3

--- react1st/react1st/src/components/ProfileCard.jsx
+++ react1st/react1st/src/components/ProfileCard.jsx
@@ -69,7 +69,7 @@ function Profile(){
         greetings = {
             <div>
                 <strong>
-                    Hi RAM, have a wonderful day
+                    Hi test , have a wonderful day
                 </strong>
             </div>
         }

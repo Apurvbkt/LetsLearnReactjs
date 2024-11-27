b5382310020889bab1fe397947ba50e6a24c2be9
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-27 20:56:15 +0530

    push 4

--- react2/src/components/CardDtl.jsx
+++ react2/src/components/CardDtl.jsx
@@ -2,7 +2,8 @@ import seriesData from '../api/netflixData.json'
 
 const CardDtl = ()=>{
       return(
-        <>
+        <ul>
+        <li>
          <div>
             <img src= {seriesData[0].img_url} alt=""  height="40%" width= "30%" />
           </div>
@@ -10,9 +11,46 @@ const CardDtl = ()=>{
           <h3>Rating: {seriesData[0].rating}</h3>
           <p>Summary: {seriesData[0].description} </p>
           <p>Genre: {seriesData[0].genre} </p>
-          <button>watch now {seriesData[0].watch_url}  </button>
-          
-          
+          <a href={seriesData[0].watch_url} target='_blank'>click to watch</a>
+          <br />
+          <button>watch now   </button>
+          </li>
+          <li>
+         <div>
+            <img src= {seriesData[1].img_url} alt=""  height="40%" width= "30%" />
+          </div>
+          <h2>Name: {seriesData[1].name}</h2>
+          <h3>Rating: {seriesData[1].rating}</h3>
+          <p>Summary: {seriesData[1].description} </p>
+          <p>Genre: {seriesData[1].genre} </p>
+          <a href={seriesData[1].watch_url} target='_blank'>click to watch</a>
+          <br />
+          <button>watch now   </button>
+          </li>
+          <li>
+         <div>
+            <img src= {seriesData[2].img_url} alt=""  height="40%" width= "30%" />
+          </div>
+          <h2>Name: {seriesData[2].name}</h2>
+          <h3>Rating: {seriesData[2].rating}</h3>
+          <p>Summary: {seriesData[2].description} </p>
+          <p>Genre: {seriesData[2].genre} </p>
+          <a href={seriesData[2].watch_url} target='_blank'>click to watch</a>
+          <br />
+          <button>watch now   </button>
+          </li>
+          <li>
+         <div>
+            <img src= {seriesData[3].img_url} alt=""  height="40%" width= "30%" />
+          </div>
+          <h2>Name: {seriesData[3].name}</h2>
+          <h3>Rating: {seriesData[3].rating}</h3>
+          <p>Summary: {seriesData[3].description} </p>
+          <p>Genre: {seriesData[3].genre} </p>
+          <a href={seriesData[3].watch_url} target='_blank'>click to watch</a>
+          <br />
+          <button>watch now   </button>
+          </li>
           {/* <button>{canWatch}</button> */}
           {/* <button>{canWatch()}</button> */}
           
@@ -25,7 +63,7 @@ const CardDtl = ()=>{
             
             
             {/* <ValueData/> */}
-        </>
+        </ul>
       )
     
     

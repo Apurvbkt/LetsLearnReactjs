a76ff72f3bfb5609e4a768bea8f0b00e43fd9160
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-27 21:08:34 +0530

    push till now , and totally understood

--- react2/src/components/CardDtl.jsx
+++ react2/src/components/CardDtl.jsx
@@ -3,54 +3,34 @@ import seriesData from '../api/netflixData.json'
 const CardDtl = ()=>{
       return(
         <ul>
-        <li>
+          { 
+            seriesData.map((e) =>{
+              return(
+                
+        <li key={e.id}>
          <div>
-            <img src= {seriesData[0].img_url} alt=""  height="40%" width= "30%" />
+            <img src= {e.img_url} alt=""  height="40%" width= "30%" />
           </div>
-          <h2>Name: {seriesData[0].name}</h2>
-          <h3>Rating: {seriesData[0].rating}</h3>
-          <p>Summary: {seriesData[0].description} </p>
-          <p>Genre: {seriesData[0].genre} </p>
-          <a href={seriesData[0].watch_url} target='_blank'>click to watch</a>
+          <h2>Name: {e.name}</h2>
+          <h3>Rating: {e.rating}</h3>
+          <p>Summary: {e.description} </p>
+          <p>Cast: {e.cast}</p>
+          <p>Genre: {e.genre} </p>
+          <a href={e.watch_url} target='_blank'>click to watch</a>
           <br />
           <button>watch now   </button>
           </li>
-          <li>
-         <div>
-            <img src= {seriesData[1].img_url} alt=""  height="40%" width= "30%" />
-          </div>
-          <h2>Name: {seriesData[1].name}</h2>
-          <h3>Rating: {seriesData[1].rating}</h3>
-          <p>Summary: {seriesData[1].description} </p>
-          <p>Genre: {seriesData[1].genre} </p>
-          <a href={seriesData[1].watch_url} target='_blank'>click to watch</a>
-          <br />
-          <button>watch now   </button>
-          </li>
-          <li>
-         <div>
-            <img src= {seriesData[2].img_url} alt=""  height="40%" width= "30%" />
-          </div>
-          <h2>Name: {seriesData[2].name}</h2>
-          <h3>Rating: {seriesData[2].rating}</h3>
-          <p>Summary: {seriesData[2].description} </p>
-          <p>Genre: {seriesData[2].genre} </p>
-          <a href={seriesData[2].watch_url} target='_blank'>click to watch</a>
-          <br />
-          <button>watch now   </button>
-          </li>
-          <li>
-         <div>
-            <img src= {seriesData[3].img_url} alt=""  height="40%" width= "30%" />
-          </div>
-          <h2>Name: {seriesData[3].name}</h2>
-          <h3>Rating: {seriesData[3].rating}</h3>
-          <p>Summary: {seriesData[3].description} </p>
-          <p>Genre: {seriesData[3].genre} </p>
-          <a href={seriesData[3].watch_url} target='_blank'>click to watch</a>
-          <br />
-          <button>watch now   </button>
-          </li>
+                
+              )
+              
+            })
+          }
+          
+          
+          
+          
+          
+          
           {/* <button>{canWatch}</button> */}
           {/* <button>{canWatch()}</button> */}
           

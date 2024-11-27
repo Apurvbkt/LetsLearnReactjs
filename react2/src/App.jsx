eb08bbf413d4a7d619b37e6f7590656241ad108a
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-27 19:52:50 +0530

    file structure done 2

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -8,8 +8,6 @@ function App() {
     <>
              
       <CardDtl/>
-      <CardDtl/>
-      <CardDtl/>
       
     </>
   )
--- react2/src/components/CardDtl.jsx
+++ react2/src/components/CardDtl.jsx
@@ -1,31 +1,24 @@
- const CardDtl = ()=>{
-    const name = 'Cat king';
-    const rating = 8.5;
-    const sum = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, iure."
-    let age = 149;
-    // let canWatch = 'not avail';
-    // if(age >= 18) canWatch = 'watch now'
-    const canWatch = ()=>{
-      
-      if(age >=18) return('Watch now')
-      return 'not avail'
-      
-    }
-      
+import seriesData from '../api/netflixData.json'
+
+const CardDtl = ()=>{
       return(
         <>
          <div>
-            <img src="https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg" alt=""  height="40%" width= "30%" />
+            <img src= {} alt=""  height="40%" width= "30%" />
           </div>
-          <h2>Name: {name}</h2>
-          <h3>Rating: {rating}</h3>
-          <p>Summary: {sum} </p>
-          <button>{age >= 18 ? 'watch now' : 'not watch'}  </button>
-          {/* <button>{canWatch}</button> */}
-          <button>{canWatch()}</button>
+          <h2>Name: {}</h2>
+          <h3>Rating: {}</h3>
+          <p>Summary: {} </p>
+          <button>{}  </button>
           
-            <h1 > <b> React #6 video completed  </b></h1> 
-            <h1 > <b> React #8 video completed  </b></h1> 
+          
+          {/* <button>{canWatch}</button> */}
+          {/* <button>{canWatch()}</button> */}
+          
+           
+            
+            
+            
             <h1 > <b> React #11 video completed  </b></h1>  
             
             
@@ -38,3 +31,20 @@
   }
   
   export default CardDtl
+  
+  
+  
+  
+  // const name = 'Cat king';
+  //   const rating = 8.5;
+  //   const sum = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, iure."
+  //   let age = 149;
+  //   // let canWatch = 'not avail';
+  //   // if(age >= 18) canWatch = 'watch now'
+  //   const canWatch = ()=>{
+      
+  //     if(age >=18) return('Watch now')
+  //     return 'not avail'
+      
+  //   }
+      

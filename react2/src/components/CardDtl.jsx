28a15c303d5533c96f4ab392b615379fd4b9a89f
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-29 22:33:13 +0530

    push 1

--- react2/src/components/CardDtl.jsx
+++ react2/src/components/CardDtl.jsx
@@ -41,6 +41,7 @@ const CardDtl = ()=>{
             <h1 > <b> React #11 video completed  </b></h1>  
             <h1 > <b> React #12 video completed  </b></h1>  
             <h1> aaj ni ho paya, coz mummy kla patna jari hai, and unke sath shopping and sara sab kuch buy krne gye the and sara saaman pack krne me time chale gya. To jo chut gya wo kal cover krunga, thank you good night</h1>
+             better 
             
             
             

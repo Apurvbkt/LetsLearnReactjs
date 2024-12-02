51758628e434c9be07998f8a6da53750c5a2ce62
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-02 11:18:33 +0530

    push 2

---
+++ react2/src/components/SeriseCard.jsx
@@ -0,0 +1,20 @@
+ export const SeriseCard = (e)=>{
+    
+    return(
+                
+        <li key={e.e.id}>
+         <div>
+            <img src= {e.e.img_url} alt=""  height="40%" width= "30%" />
+          </div>
+          <h2>Name: {e.e.name}</h2>
+          <h3>Rating: {e.e.rating}</h3>
+          <p>Summary: {e.e.description} </p>
+          <p>Cast: {e.e.cast}</p>
+          <p>Genre: {e.e.genre} </p>
+          <a href={e.e.watch_url} target='_blank'>click to watch</a>
+          <br />
+          <button>watch now   </button>
+          </li>
+                
+              )
+ }

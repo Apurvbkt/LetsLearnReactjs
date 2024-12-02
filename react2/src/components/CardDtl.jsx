60dc7bcdb2015fc9aa3797b47fafff146314a557
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-02 11:18:28 +0530

    start day 13

--- react2/src/components/CardDtl.jsx
+++ react2/src/components/CardDtl.jsx
@@ -1,27 +1,12 @@
 import seriesData from '../api/netflixData.json'
-
+import { SeriseCard } from './SeriseCard'
 const CardDtl = ()=>{
       return(
         <ul>
           { 
             seriesData.map((e) =>{
-              return(
-                
-        <li key={e.id}>
-         <div>
-            <img src= {e.img_url} alt=""  height="40%" width= "30%" />
-          </div>
-          <h2>Name: {e.name}</h2>
-          <h3>Rating: {e.rating}</h3>
-          <p>Summary: {e.description} </p>
-          <p>Cast: {e.cast}</p>
-          <p>Genre: {e.genre} </p>
-          <a href={e.watch_url} target='_blank'>click to watch</a>
-          <br />
-          <button>watch now   </button>
-          </li>
-                
-              )
+             
+            <SeriseCard e = {e}/>
               
             })
           }
@@ -40,8 +25,7 @@ const CardDtl = ()=>{
             
             <h1 > <b> React #11 video completed  </b></h1>  
             <h1 > <b> React #12 video completed  </b></h1>  
-            <h1>kal 13 wli video complete krni hai, baaki ka dheere dheere speed up krunga </h1>
-            good night 
+           
             
             {/* <ValueData/> */}
         </ul>

c2c04026da7264b91ad8dcc2123f604df980cdba
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-03 20:29:02 +0530

    push 1

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -1,19 +1,19 @@
  export const SeriseCard = (props)=>{
     console.log(props);
-    
+    const {e} = props
     
     return(
                 
         <li>
          <div>
-            <img src= {props.e.img_url} alt=""  height="40%" width= "30%" />
+            <img src= {e.img_url} alt=""  height="40%" width= "30%" />
           </div>
-          <h2>Name: {props.e.name}</h2>
-          <h3>Rating: {props.e.rating}</h3>
-          <p>Summary: {props.e.description} </p>
-          <p>Cast: {props.e.cast}</p>
-          <p>Genre: {props.e.genre} </p>
-          <a href={props.e.watch_url} target='_blank'>click to watch</a>
+          <h2>Name: {e.name}</h2>
+          <h3>Rating: {e.rating}</h3>
+          <p>Summary: {e.description} </p>
+          <p>Cast: {e.cast}</p>
+          <p>Genre: {e.genre} </p>
+          <a href={e.watch_url} target='_blank'>click to watch</a>
           <br />
           <button>watch now   </button>
           </li>

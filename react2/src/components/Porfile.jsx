541defde86bbb4380d8b7524823d2086f52e292b
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-04 19:32:24 +0530

    psuh 2

---
+++ react2/src/components/Porfile.jsx
@@ -0,0 +1,20 @@
+function Profile(){
+    return(
+        <div>
+            <h1>Profile Card challenge</h1>
+            <ProfileCard
+            name = "Apurv"
+            age = {23}
+            greeting = {
+                <div>
+                    <strong>
+                        Hii Apurv, Have a great day
+                    </strong>
+                </div>                
+            }>
+                <p>Hobbies:  Reading, tech</p>
+                <button>Contact</button>
+            </ProfileCard>
+        </div>
+    )
+}

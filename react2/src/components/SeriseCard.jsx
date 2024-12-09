d3046cf65f5a78b89e9b5c15adf4ed0ff2067b9e
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-09 21:29:08 +0530

    push 6

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -23,7 +23,7 @@
           aaj bhi rest kyuki aaj man ni krra hai kal se krunga, aaj mind thoda disturb hai 
           
           weekend ko aaram , week days ko kam 
-          baaki ka mai, kal krunga, good night, not in good mood, karunga pakka karunga, thoda time ni mil para hai , but for sure
+          baaki ka mai, kal krunga, good night, not in good mood, karunga pakka karunga, thoda time ni mil para hai , but for sure krunga
           </li>
                 
               )

a05b3de063029b5a184db8c1bbc1626fc0fede00
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-15 21:56:28 +0530

    push 4

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -30,7 +30,8 @@
           
           as your know, pdhai ke time wla drama, har bar ka wohi hai, pata ni aise kaise chalega, seriouly I freaked out. ye sab drama se chutti mile to jake pdhai ho. see u all tomorrow . Bye good night
           
-          we went for a movie today, and also I missed one class, this time my attendance will go down, coz already missed my two classes
+          we went for a movie today, and also I missed one class, this time my attendance will go down, coz already missed my two classes.
+          also it's an amazing day
           </li>
                 
               )

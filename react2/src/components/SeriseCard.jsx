058813429d16a2e2cfeb9f4bc5ed725f4bc54a92
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-19 19:27:11 +0530

    push

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -26,12 +26,12 @@
           baaki ka mai, kal krunga, good night, not in good mood, karunga pakka karunga, thoda time ni mil para hai , but for sure krunga. for now good night
           every thing is going not up to the plan, sayd thoda break lena he best hoga, kyuki sara kam khtm krte he aaram se coding krnge , good night 
           dadu expire, ab manhara jana padega, to aaj kl push krna thoda dikkt hai , baaki mai jaisa hoga waisa mai bolunga
-          krta hun , avi thoda baitha hun, ghr aake krunga aaram se, thank you, for understanding, aaj maine adi se baat krra busness ke bare let's take a risk and roll, agar chal jata hai business to well and good, and agar ni chalta hai, to back to kam kaaj, hoping hmlog ache se kr paye sab kuch. bye goodnight
+          krta hun , avi thoda baitha hun, ghr aake krunga aaram se, thank you, for understanding, aaj maine adi se baat krra busness ke bare let&apos;s take a risk and roll, agar chal jata hai business to well and good, and agar ni chalta hai, to back to kam kaaj, hoping hmlog ache se kr paye sab kuch. bye goodnight
           
           as your know, pdhai ke time wla drama, har bar ka wohi hai, pata ni aise kaise chalega, seriouly I freaked out. ye sab drama se chutti mile to jake pdhai ho. see u all tomorrow . Bye good night
           
           we went for a movie today, and also I missed one class, this time my attendance will go down, coz already missed my two classes.
-          also it's an amazing day, mama ji brought new phone, and that is one plus 9pro. It was my fav phone in 2021, but unfortunatly mere kismat me nahi tha. but coding i guess ab suru hoga nana ji ke bhoj ke baad, so stay tune. good night
+          also it&apos;s an amazing day, mama ji brought new phone, and that is one plus 9pro. It was my fav phone in 2021, but unfortunatly mere kismat me nahi tha. but coding i guess ab suru hoga nana ji ke bhoj ke baad, so stay tune. good night
            
            It was a good day, aaj invite krne gye, to bahut thak gye, and sahi rha din, pdhai suru krnge, aaj ke liye etna he. good night
            
@@ -39,6 +39,8 @@
           bye, good night
           
           aaj ka din normal tha. today i went to the gym, and also had a great day, wish kal bhi thik ho and sab ache se ho jaye, good night
+          
+          hey github, 
           </li>
                 
               )

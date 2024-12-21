86b2d2434ec48999bb231ca7c9a67de45bac60cd
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-12-21 23:02:08 +0530

    push 4

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -45,7 +45,7 @@
           
           hey, github it was a good day, aaj ghat tha. aaj sab proper sab function hua , ab bas kal ka din uske baad sab end. uske baad back to track.
           
-          it was a great day, had a wonderful 
+          it was a great day, had a wonderful
           
           </li>
                 

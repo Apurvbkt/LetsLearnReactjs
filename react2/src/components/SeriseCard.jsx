c35bab074185bd75dc863e1096319935c0fa16d9
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-01-03 20:00:11 +0530

    push 3

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -76,7 +76,7 @@
         
         push 1, aaj gym krke clories dificiet ni krpaya. push 3, good night
         
-        hey, aaj bhi calories deficit me tha and u know what, acha gya ab tkk
+        hey, aaj bhi calories deficit me tha and u know what, acha gya ab tkk, hopefully sab aise he chalte jayega
           </li>
                 
               )

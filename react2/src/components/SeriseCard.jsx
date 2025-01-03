54bd8d761808659a9493d0616e72a2f9846efb1a
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-01-03 19:59:22 +0530

    psuh 1

--- react2/src/components/SeriseCard.jsx
+++ react2/src/components/SeriseCard.jsx
@@ -75,6 +75,8 @@
         . it was fun. sbse mile hii hello, sab aaye the maja aaya. 1st day acha gya. i hope aaj muje thoda race dekhne ko mila , may be ab life me aise he race ho or sbse aage badne ka junun mje sbse aage leke jayega . hoping for best good night
         
         push 1, aaj gym krke clories dificiet ni krpaya. push 3, good night
+        
+        hey,
           </li>
                 
               )
